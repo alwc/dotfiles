be8e7ec1dfc8bbc69d2f7b0ad66f8e19d010f776
Alex L <alex90@gmail.com>
2018-10-20 16:20:55 +0800

    update Brewfile

--- install_new.sh
+++ install_new.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 # 1. Install Homebrew
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# https://brew.sh/
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# 2. Install git then clone dotfiles to home directory
+brew update && brew upgrade && brew install git
+git clone git@github.com:alwc/dotfiles.git
+
+# 3. Symlink dotfiles
 DOTFILES_DIR=~/dotfiles
 CONFIG_DIR=~/.config
 
-# ln -s $DOTFILES_DIR/karabiner $CONFIG_DIR/karabiner
-# ln -s $DOTFILES_DIR/kitty $CONFIG_DIR/kitty
-# ln -s $DOTFILES_DIR/nvim $CONFIG_DIR/nvim
-
-# ln -s $DOTFILES_DIR/bash_profile ~/.bash_profile
+ln -s $DOTFILES_DIR/karabiner $CONFIG_DIR/karabiner
+ln -s $DOTFILES_DIR/kitty $CONFIG_DIR/kitty
+ln -s $DOTFILES_DIR/nvim $CONFIG_DIR/nvim
+ln -s $DOTFILES_DIR/bash_profile ~/.bash_profile
 ln -s $DOTFILES_DIR/osx/bashrc ~/.bashrc
-# ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
-# ln -s $DOTFILES_DIR/latexmkrc ~/.latexmkrc
+ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
+ln -s $DOTFILES_DIR/latexmkrc ~/.latexmkrc
 ln -s $DOTFILES_DIR/osx/profile ~/.profile
-# ln -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
-# ln -s $DOTFILES_DIR/vimrc ~/.vimrc
+ln -s $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES_DIR/vimrc ~/.vimrc
 
-# 3. Install Tmux Plugin Manager (TPM)
+# 4. Install Tmux Plugin Manager (TPM)
+# https://github.com/tmux-plugins/tpm#installing-plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
