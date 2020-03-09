20a64b06a163dfe293e77f9f172c200146dbc7bf
Alex L <alex90@gmail.com>
2020-03-09 23:28:41 +0800

    Remove old osx settings:

--- osx-settings.sh
+++ osx-settings.sh
@@ -17,9 +17,3 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Automatically quit printer app once the print jobs complete
 # defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-# Specify iTerm2's preferences directory
-# defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/"
-
-# Tell iTerm2 to use the custom preferences in the directory
-# defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
